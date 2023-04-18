2b9d2a8d67aeb979fab74f6bf33849e666ebcb21
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-18 16:26:32 +0200

    practicing with eslint

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {});
+let name = "John";
+
+function printName() {
+    console.log(name);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i +" " + name);
+}
