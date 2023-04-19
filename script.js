e82ba30e44a30dea273a33c5e24f608542882811
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-19 17:53:23 +0200

    js revision

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const myFirstObject = {
-  property: "Value!",
-  secondProperty: 420,
-  "Third Property": function() {
-    console.log(myFirstObject.secondProperty);
-  }
-};
+  name:{
+    first: 'Bob',
+    second: 'Smith',
+  },
+  age: 69,
+  bio() {
+    console.log(`${this.name.first} ${this.name.second} is ${this.age} years old`);
+  },
+  introduceSelf() {
+    console.log(`Hi, I'm ${this.name[0]} ${this.name[1]}.`);
+  },
+}; 
