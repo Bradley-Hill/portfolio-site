49e0f194e359fdbee43941b9552936b8a65c07ef
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-20 16:36:14 +0200

    good god

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-
 function Cat(name, breed, color) {
   this.name = name;
   this.breed = breed;
   this.color = color;
-  this.greeting = function() {
+  this.greeting = function () {
     return `Hello, said ${this.name} the ${this.breed}.`;
-  }
+  };
 }
 
-Cat3= new Cat('Rupert', 'Siamese', 'white');
-Cat4 =new Cat('Jenkins', 'Moggy', 'ginger');
+Cat3 = new Cat('Rupert', 'Siamese', 'white');
+Cat4 = new Cat('Jenkins', 'Moggy', 'ginger');
 
 console.log(Cat3.greeting());
 console.log(Cat4.greeting());
 
-var unusedVariable = 42;  
+const unusedVariable = 42; 
