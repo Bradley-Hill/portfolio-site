870f15b4c45ef0d8e4d3803fb11490d54fb9da02
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-20 15:48:11 +0200

    attempting to config eslint

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const myFirstObject = {
-  name:{
-    first: 'Bob',
-    second: 'Smith',
-  },
-  age: 69,
-  bio() {
-    console.log(`${this.name.first} ${this.name.second} is ${this.age} years old`);
-  },
-  introduceSelf() {
-    console.log(`Hi, I'm ${this.name[0]} ${this.name[1]}.`);
-  },
-}; 
+
+function Cat(name, breed, color) {
+  this.name = name;
+  this.breed = breed;
+  this.color = color;
+  this.greeting = function() {
+    return `Hello, said ${this.name} the ${this.breed}.`;
+  }
+}
+
+Cat3= new Cat('Rupert', 'Siamese', 'white');
+Cat4 =new Cat('Jenkins', 'Moggy', 'ginger');
+
+console.log(Cat3.greeting());
+console.log(Cat4.greeting());
+
+var unusedVariable = 42;  
